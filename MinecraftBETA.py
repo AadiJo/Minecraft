225827283426f665739701f15afd065d7ee0dd1b
AadiJo <79769495+AadiJo@users.noreply.github.com>
2021-03-04 10:25:34 -0600

    Update MinecraftBETA.py

--- MinecraftBETA.py
+++ MinecraftBETA.py
@@ -3,13 +3,13 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 
 app = Ursina()
-grass_texture = load_texture('assets/grass_block.png')
-stone_texture = load_texture('assets/stone_block.png')
-brick_texture = load_texture('assets/brick_block.png')
-dirt_texture  = load_texture('assets/dirt_block.png')
-sky_texture   = load_texture('assets/skybox.png')
-arm_texture   = load_texture('assets/arm_texture.png')
-punch_sound   = Audio('assets/punch_sound', loop = False, autoplay = False)
+grass_texture = load_texture('grass_block.png')
+stone_texture = load_texture('stone_block.png')
+brick_texture = load_texture('brick_block.png')
+dirt_texture  = load_texture('dirt_block.png')
+sky_texture   = load_texture('skybox.png')
+arm_texture   = load_texture('arm_texture.png')
+punch_sound   = Audio('punch_sound', loop = False, autoplay = False)
 block_pick = 1
 
 window.fps_counter.enabled = False
